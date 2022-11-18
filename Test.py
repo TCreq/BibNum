589084ac2ac3511656a364ce55a2138c166b7e17
TCreq <tudalcrequy@gmail.com>
2022-11-19 00:15:33 +0100

    remise à jour

--- Test.py
+++ Test.py
@@ -15,12 +15,42 @@ import epub
 
 book = epub.open_epub('stendhal_-_le_rouge_et_le_noir.epub')
 
+
+opf0=book.opf
+meta=opf0.metadata
+# meta est un objet de la classe Metadata contenant plusieurs titres
+title=meta.titles[0][0]
+print(f"le titre du livre est : {title}")
+
+
+toc0=book.toc
+print(toc0.authors)
+print(toc0.__class__.__name__)
+print(toc0.nav_map)
+#print(toc0.nav_map.nav_point)
+print("stop")
+#print(i.class_name for i in toc0.nav_map.nav_point)
+
+
+
+
+k=0
 for item_id, linear in book.opf.spine.itemrefs:
+    k+=1
     item = book.get_item(item_id)
     # Check if linear or not
     if linear:
-        print('Linear item "%s"' % item.href)
+        #print('Linear item "%s"' % item.href)
+        pass
     else:
-        print('Non-linear item "%s"' % item.href)
+        #print('Non-linear item "%s"' % item.href)
+        pass
     # read the content
     data = book.read_item(item)
+    if k==3 :
+        #print(data)
+        pass
+
+
+
+#print(book.opf.spine.itemrefs[0])
