e976078f3657b21d8abd421ff29cd231ad3f0fce
TCreq <tudalcrequy@gmail.com>
2022-11-21 11:10:26 +0100

    test rapports

--- Livre.py
+++ Livre.py
@@ -72,7 +72,7 @@ with open("rapport.txt","w") as f:
 my_canvas = canvas.Canvas("rapport.pdf")
 k=0
 for i in l.specpdf():
-  my_canvas.drawString(100, 750+k*15, str(i))
+  my_canvas.drawString(100, 750-k*15, str(i))
   k+=1
 my_canvas.save()
 
