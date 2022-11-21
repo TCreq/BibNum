a9e8e2b198147368bd5d7a1db0321cc7940dcc53
TCreq <tudalcrequy@gmail.com>
2022-11-21 12:07:03 +0100

    test rapports

--- Livre.py
+++ Livre.py
@@ -8,8 +8,9 @@
 #import copy #Pour copier différentes listes ou objets
 from PyPDF2 import PdfReader
 import epub as e2
+import ebooklib
 from ebooklib import epub
-#from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup
 from langdetect import detect
 from reportlab.pdfgen import canvas
 
@@ -69,6 +70,7 @@ with open("rapport.txt","w") as f:
   print(str(l), file=f)
 
 
+'''
 my_canvas = canvas.Canvas("rapport.pdf")
 k=0
 for i in l.specpdf():
@@ -79,4 +81,127 @@ for i in l.specpdf():
   k+=1
 my_canvas.showPage()
 my_canvas.save()
+'''
+
+"""
+book = epub.EpubBook()
+book.set_identifier('rapport'+l.titre)
+book.set_title(l.titre)
+book.set_language(l.lang)
+book.add_author(l.auteur)
+soup=BeautifulSoup()
+body=soup.new_tag('body')
+soup.insert(0,body)
+s=l.specpdf()
+s.reverse()
+for line in s:
+  body.insert(0,line)
+# intro chapter
+c1 = epub.EpubHtml(title='Rapport',
+                   file_name='Rapport.xhtml',
+                   lang='fr')
+c1.set_content(soup.prettify())
+#print(c1.get_content())
+book.add_item(c1)
+style = 'body { font-family: Times, Times New Roman, serif; }'
+
+nav_css = epub.EpubItem(uid="style_nav",
+                        file_name="style/nav.css",
+                        media_type="text/css",
+                        content=style)
+book.add_item(nav_css)
+book.add_item(epub.EpubNcx())
+book.add_item(epub.EpubNav())
+epub.write_epub('test.epub', book)
+
+
+
+
+with open('rapport.html', 'w') as outfile:
+    outfile.write(soup.prettify())
+"""
+
+book = epub.EpubBook()
+book.set_identifier('sample123456')
+book.set_title('Sample book')
+book.set_language('en')
+
+book.add_author('Aleksandar Erkalovic')
+book.add_metadata('DC', 'description', 'This is description for my book')
+book.add_metadata(None, 'meta', '', {'name': 'key', 'content': 'value'})
+
+# intro chapter
+c1 = epub.EpubHtml(title='Introduction',
+                   file_name='intro.xhtml',
+                   lang='en')
+c1.set_content(u'<html><body><h1>Introduction</h1><p>Introduction paragraph.</p></body></html>')
+
+# about chapter
+c2 = epub.EpubHtml(title='About this book',
+                   file_name='about.xhtml')
+c2.set_content('<h1>About this book</h1><p>This is a book.</p>')
+
+
+#print(c1.get_content())
+
+
+book.add_item(c1)
+book.add_item(c2)
+
+style = 'body { font-family: Times, Times New Roman, serif; }'
+
+nav_css = epub.EpubItem(uid="style_nav",
+                        file_name="style/nav.css",
+                        media_type="text/css",
+                        content=style)
+book.add_item(nav_css)
+
+book.toc = (epub.Link('intro.xhtml', 'Introduction', 'intro'),
+              (
+                epub.Section('Languages'),
+                (c1, c2)
+              )
+            )
+book.spine = ['nav', c1, c2]
+
+book.add_item(epub.EpubNcx())
+book.add_item(epub.EpubNav())
+
+epub.write_epub('test.epub', book)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
