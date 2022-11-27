e08733a3f101d225a358508a673a3763d6c6e097
TCreq <tudalcrequy@gmail.com>
2022-11-27 15:58:54 +0100

    rapport ouvrages

--- bibli.py
+++ bibli.py
@@ -154,10 +154,54 @@ for i in c:
   lprops[k]=" ".join(i.props())
   k+=1
 texte="\n".join(lprops)
+tepub='<br />'.join(lprops)
 print(texte)
 
 with open(rapports+"ouvrages.txt","w") as f:
   print(texte, file=f)
+
+my_canvas = canvas.Canvas(rapports+"ouvrages.pdf")
+k=0
+for i in lprops:
+  my_canvas.drawString(100, 750-k*15, str(i))
+  if 750-k*15<100:
+    my_canvas.showPage()
+    k=0
+  k+=1
+my_canvas.showPage()
+my_canvas.save()
+
+
+
+book = epub.EpubBook()
+book.set_identifier('Ouvrages')
+book.set_title('Ouvrages')
+book.set_language('fr')
+book.add_author('system')
+book.add_metadata('DC', 'description', 'Ouvrages de la bibliotheque')
+book.add_metadata(None, 'meta', '', {'name': 'key', 'content': 'value'})
+c1 = epub.EpubHtml(title='Ouvrages',
+                 file_name='prop.xhtml',
+                 lang='fr')
+c1.set_content('<html><body><h1>Ouvrages</h1><p>'+tepub+'</p></body></html>')
+c2 = epub.EpubHtml(title='Fin',
+                 file_name='fin.xhtml')
+c2.set_content('<h1> ... </h1><p> ... </p>')
+book.add_item(c1)
+book.add_item(c2)
+style = 'body { font-family: Times, Times New Roman, serif; }'
+nav_css = epub.EpubItem(uid="style_nav",
+                      file_name="style/nav.css",
+                      media_type="text/css",
+                      content=style)
+book.add_item(nav_css)
+book.toc = (c1,c2)
+book.spine = ['nav', c1, c2]
+book.add_item(epub.EpubNcx())
+book.add_item(epub.EpubNav())
+epub.write_epub(rapports+'ouvrages.epub', book)
+
+
 
 ### Liste des Auteurs
 # la liste des auteurs (au format texte, PDF et epub)
